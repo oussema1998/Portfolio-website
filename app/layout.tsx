8c9049ebdb1cd5fc604da25f69d7098ebc371b03
oussema1998 <oussemaxbox360@gmail.com>
2026-04-29 19:16:37 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { SitePreferencesProvider } from "./context/SitePreferencesContext";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -49,6 +51,7 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <Footer />
         </SitePreferencesProvider>
+        <Analytics />
       </body>
     </html>
   );
